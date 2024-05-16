c8d7f3ca228872a63f5a0a57f865522d3a3ad316
andresalbo <alboandres@gmail.com>
2024-05-16 05:42:40 -0300

    bug fixes

--- src/app/modules/abm/abm-lots/lots/lots.component.ts
+++ src/app/modules/abm/abm-lots/lots/lots.component.ts
@@ -375,6 +375,7 @@ export class LotsComponent implements OnInit, AfterViewInit, OnDestroy {
           return 0;
       }
     });
+    this.dataSource.data = this.sortedData
   }
   compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
