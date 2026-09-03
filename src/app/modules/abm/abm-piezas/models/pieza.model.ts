9a8e895e2d3c41b3632cb7839d02b24aa2d7182e
Cristian Aguedo <cristian.aguedo@gmail.com>
2026-09-03 19:23:50 -0300

    Fix Clonar: Material > Formula

--- src/app/modules/abm/abm-piezas/models/pieza.model.ts
+++ src/app/modules/abm/abm-piezas/models/pieza.model.ts
@@ -91,8 +91,8 @@ export interface PiezaClonarDTO {
     idPiezaOriginal: number;
     codigo: string;
     nombre: string;
-    idMaterial?: number | null;
-    material?: string | null;
+    idFormula?: number | null;
+    formula?: string | null;
     idCliente: number;
     nombrePiezaPersonalizado?: string | null;
     cotizacion?: number | null;
